8ded18fdc24cfa73989479d6545b1a4483c1ea1d
Michael Metyk <126725193+breadforhire@users.noreply.github.com>
2024-11-27 22:32:12 -0500

    Update main.cpp

--- 5x5 tictactoe/main.cpp
+++ 5x5 tictactoe/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 vector<vector<char>> board(sz, vector<char> (sz,'_'));
 vector<vector<string>> color(sz, vector<string> (sz, "White"));
+string player = "Player 1";
 // 2-d vector to signify color
 
 signed main(){
@@ -17,7 +18,7 @@ signed main(){
     print_board(board, color);
 
     if(player_style == 1) one_player(board,color);
-    else multiplayer(board, color);
+    else multiplayer(board, color, player);
+
 
 }
-
